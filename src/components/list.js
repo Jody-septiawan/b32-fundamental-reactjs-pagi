b4a20ff614bcf9f20a44d6df4ed4f1cf847e438a
Jody-septiawan <jody.septiawan5@gmail.com>
2022-03-28 11:00:37 +0700

    Props

--- src/components/list.js
+++ src/components/list.js
@@ -1,1 +1,11 @@
-// code Here
+export default function List(props) {
+  console.log(props);
+
+  return (
+    <div>
+      <h1>{props.nama}</h1>
+      <h4>{props.year}</h4>
+      <button onClick={props.handleClick}>Cmick Me</button>
+    </div>
+  );
+}
--- src/props.js
+++ src/props.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React from 'react';
 
 //Import Components
+import List from './components/list';
 
 function Props() {
+  const car = 'BMW White';
+  const color = 'White';
+  const year = 2017;
+  const detailCar = {
+    author: 'Mr. Alvin',
+    desc: 'The purpose of a gasoline car engine is to convert gasoline into motion so that your car can move. Currently the easiest way to create motion from gasoline is to burn the gasoline inside an engine. Therefore, a car engine is an internal combustion engine — combustion takes place internally.',
+  };
+
+  function handleClick() {
+    alert('Hello World');
+  }
+
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <List nama={car} color={color} year={year} detailCar={detailCar} />
+      <List nama="Mercedes-Benz" />
+      <List nama="Ducati" handleClick={handleClick} />
+    </div>
   );
 }
 
