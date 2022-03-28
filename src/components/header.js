b66c5a62e673eb3888b3251bc62d6dadaac8a643
Jody-septiawan <jody.septiawan5@gmail.com>
2022-03-28 10:29:06 +0700

    Component

--- src/components/header.js
+++ src/components/header.js
@@ -1,1 +1,15 @@
-// Code Here
+// Class Component
+// Function Component
+
+function Header() {
+  return (
+    <div>
+      <h1>This is Header</h1>
+      <a href="">Home</a>
+      <a href="">About</a>
+      <a href="">Contact Me</a>
+    </div>
+  );
+}
+
+export default Header;
